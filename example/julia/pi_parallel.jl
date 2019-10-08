51a90d1e37edf658c3697cedee4838048ef11ce0
wupeifan <wupeifan@gmail.com>
2019-10-08 15:15:29 -0700

    use @distributed instead of pmap for small tasks

--- example/julia/pi_parallel.jl
+++ example/julia/pi_parallel.jl
@@ -9,16 +9,22 @@ a = 0.0
 b = 1.0
 dx = (b - a) / (n - 1)
 
-cur_range = range(a, b, step = dx)
-tmpres = pmap(myf, cur_range)
+# cur_range = range(a, b, step = dx)
+# tmpres = pmap(myf, cur_range)
+# s = sum(tmpres)
 
-s = sum(tmpres)
+s = 0.0
+s = @distributed (+) for i = 1:n
+    x = (i - 1) * dx + a
+    myf(x)
+end
+
 s = (s - 0.5 * (myf(a) + myf(b))) * dx
 return s
 
 end
 
-N = 65536
+N = 33554432
 for i = 1 : 50
     @time tmp = mypi_par(N)
 end
