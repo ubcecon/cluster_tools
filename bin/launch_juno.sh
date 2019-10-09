e8d24bdb989f5d19cb17f973bf77890644fb83e0
wupeifan <wupeifan@gmail.com>
2019-10-09 12:41:26 -0700

    minor updates

--- bin/launch_juno.sh
+++ bin/launch_juno.sh
@@ -1,2 +1,3 @@
-module load gcc/7.3.0 julia/1.2.0
-julia "$@"
+cd scratch
+chmod +x ~/cluster_tools/bin/setup_julia.sh
+salloc --time=1:0:0 --ntasks=1 --cpus-per-task=2 srun ~/cluster_tools/bin/setup_julia.sh
---
+++ bin/setup_julia.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+module load gcc/7.3.0 julia/1.2.0
+julia "$@"
